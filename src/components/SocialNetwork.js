c8422c26f2a8e4e19a0e20a71a464d1a9e7b97a1
bno4 <bnduchemin@gmail.com>
2023-10-27 18:28:57 +0200

    1st commit

---
+++ src/components/SocialNetwork.js
@@ -0,0 +1,63 @@
+import React from "react";
+
+const SocialNetwork = () => {
+  const anim = () => {
+    const icons = document.querySelectorAll(".social-network a");
+    icons.forEach((link) => {
+      link.addEventListener("mouseover", (e) => {
+        link.style.transform = `translate(${e.offsetX - 20}px, ${
+          e.offsetY - 13
+        }px)`;
+      });
+      link.addEventListener("mouseleave", () => {
+        link.style.transform = "";
+      });
+    });
+  };
+  return (
+    <div className="social-network">
+      <ul className="content">
+        {/* Facebook */}
+        <a
+          href="https://facebook.com"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="hover"
+          onMouseOver={anim}
+        >
+          <li>
+            <i className="fab fa-facebook-f"></i>
+          </li>
+        </a>
+
+        {/* Twitter */}
+        <a
+          href="https://twitter.com"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="hover"
+          onMouseOver={anim}
+        >
+          <li>
+            <i className="fab fa-twitter"></i>
+          </li>
+        </a>
+
+        {/* Insta————————————————————————————— */}
+        <a
+          href="https://instagram.com"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="hover"
+          onMouseOver={anim}
+        >
+          <li>
+            <i className="fab fa-instagram"></i>
+          </li>
+        </a>
+      </ul>
+    </div>
+  );
+};
+
+export default SocialNetwork;
